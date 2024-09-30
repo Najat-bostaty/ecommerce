2f34a16fabce49e3f5e8d0963c01f1e18c30b2ee
Najat-bostaty <nanabostaty2003@gmail.com>
2024-09-30 21:24:09 +0300

    product page  done

--- src/index.js
+++ src/index.js
@@ -12,4 +12,23 @@ document.querySelectorAll('.add-to-card-btn').forEach(item => {
    })   
 })
  
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item =>{
+   item.addEventListener('change', () => {
+      document.querySelectorAll('.size-option').forEach(i => {
+         i.classList.remove('active')
+      })
+      item.parentNode.parentNode.classList.add('active')
+   })
+})
+
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item =>{
+   item.addEventListener('change', () => {
+      document.querySelectorAll('.color-option').forEach(i => {
+         i.classList.remove('active')
+      })
+      item.parentNode.parentNode.classList.add('active')
+   })
+})
+
+
 document.getElementById("copyright").innerHTML = " جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear();
